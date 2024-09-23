5b682fa7d35760a7f5a932b301dbb4a8aa915882
jenifaa <jefrahman32@gmail.com>
2024-09-24 00:54:36 +0600

    added the js part

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,52 +1,81 @@
 document.getElementById('donation-btn').addEventListener('click',function(){
-   const donation = getInputFieldValueById('donation-amount');
-   const amount = parseFloat(donation);
-  
-   const current = getInputTextValueById('available-balance');
-   const currentAmount = parseFloat(current);
-   const balance = amount + currentAmount;
-   document.getElementById('available-balance').innerText = balance;
+    const donation = getInputFieldValueById('donation-amount');
+    const amount = parseFloat(donation);
 
-   const main = getInputTextValueById('main-balance');
-   const mainBalance = parseFloat(main);
-   const currenBalance = mainBalance - donation;
-   document.getElementById('main-balance').innerText = currenBalance;
-
-  
-})
-
-
-document.getElementById('donation-btn2').addEventListener('click',function(){
-    const donation2 = getInputFieldValueById('donation-amount2');
-    const amount2 = parseFloat(donation2);
+    if(isNaN(amount))
+    {
+        alert('Wrong input.Try Again.');
+        return ;
+    }
    
-    const current2 = getInputTextValueById('available-balance2');
-    const currentAmount2 = parseFloat(current2);
-    const balance2 = amount2 + currentAmount2;
-    document.getElementById('available-balance2').innerText = balance2;
+    const current = getInputTextValueById('available-balance');
+    const currentAmount = parseFloat(current);
+
+    const balance = amount + currentAmount;
+    document.getElementById('available-balance').innerText = balance;
  
     const main = getInputTextValueById('main-balance');
     const mainBalance = parseFloat(main);
-    const currenBalance2 = mainBalance - donation2;
-    document.getElementById('main-balance').innerText = currenBalance2;
+    if(amount > mainBalance)
+        {
+            alert('Wrong input.Try Again.');
+            return ;
+    
+        }
+    const currentBalance = mainBalance - donation;
+    document.getElementById('main-balance').innerText = currentBalance;
  
    
  })
-
-
-document.getElementById('donation-btn3').addEventListener('click',function(){
-    const donation3 = getInputFieldValueById('donation-amount3');
-    const amount3 = parseFloat(donation3);
-   
-    const current3 = getInputTextValueById('available-balance3');
-    const currentAmount3 = parseFloat(current3);
-    const balance3 = amount3 + currentAmount3;
-    document.getElementById('available-balance3').innerText = balance3;
  
-    const main = getInputTextValueById('main-balance');
-    const mainBalance = parseFloat(main);
-    const currenBalance3 = mainBalance - donation3;
-    document.getElementById('main-balance').innerText = currenBalance3;
  
-   
- })
+ document.getElementById('donation-btn2').addEventListener('click',function(){
+     const donation2 = getInputFieldValueById('donation-amount2');
+     const amount2 = parseFloat(donation2);
+    
+     const current2 = getInputTextValueById('available-balance2');
+     const currentAmount2 = parseFloat(current2);
+     const balance2 = amount2 + currentAmount2;
+     document.getElementById('available-balance2').innerText = balance2;
+  
+     const main = getInputTextValueById('main-balance');
+     const mainBalance = parseFloat(main);
+     const currenBalance2 = mainBalance - donation2;
+     document.getElementById('main-balance').innerText = currenBalance2;
+  
+    
+  })
+ 
+ 
+ document.getElementById('donation-btn3').addEventListener('click',function(){
+     const donation3 = getInputFieldValueById('donation-amount3');
+     const amount3 = parseFloat(donation3);
+    
+     const current3 = getInputTextValueById('available-balance3');
+     const currentAmount3 = parseFloat(current3);
+     const balance3 = amount3 + currentAmount3;
+     document.getElementById('available-balance3').innerText = balance3;
+  
+     const main = getInputTextValueById('main-balance');
+     const mainBalance = parseFloat(main);
+     const currenBalance3 = mainBalance - donation3;
+     document.getElementById('main-balance').innerText = currenBalance3;
+  
+    
+  })
+ 
+ 
+  document.getElementById('history-btn').addEventListener('click', function(){
+    document.getElementById('donation-card').classList.add('hidden');
+
+   const historyDiv = document.createElement('div');
+   historyDiv.className = "border rounded-xl p-6 mt-5";
+    historyDiv.innerHTML = `
+        
+            <p class="font-bold mb-3">96500 Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+            <p class="text-gray-500">${new Date().toLocaleDateString()}</p>
+    `;
+    document.getElementById('history').appendChild(historyDiv);
+ 
+ });
+ 
---
+++ js/features.js
@@ -0,0 +1,19 @@
+//document.getElementById('show-donation').addEventListener('click',function(){
+   
+    //document.getElementById('history').classList.add('hidden');
+    //document.getElementById('donation-card').classList.remove('hidden');
+    //document.getElementById('show-donation').classList.add('bg-button-colore');
+    //document.getElementById('history-btn').classList.remove('bg-button-colore');
+//})
+
+
+
+//document.getElementById('history-btn').addEventListener('click',function(){
+
+   // document.getElementById('donation-card').classList.add('hidden');
+    //document.getElementById('show-donation').classList.remove('bg-button-colore');
+    //document.getElementById('show-donation').classList.add('border');
+   // document.getElementById('history').classList.remove('hidden');
+   // document.getElementById('history-btn').classList.add('bg-button-colore');
+
+//})
