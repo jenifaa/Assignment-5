dc61f45bfb1889eb61160a4911aec154ddce2a02
jenifaa <jefrahman32@gmail.com>
2024-09-25 01:43:59 +0600

    added the history part

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,3 +1,4 @@
+
 document.getElementById('donation-btn').addEventListener('click',function(){
     const donation = getInputFieldValueById('donation-amount');
     const amount = parseFloat(donation);
@@ -8,40 +9,83 @@ document.getElementById('donation-btn').addEventListener('click',function(){
         return ;
     }
    
-    const current = getInputTextValueById('available-balance');
-    const currentAmount = parseFloat(current);
 
-    const balance = amount + currentAmount;
-    document.getElementById('available-balance').innerText = balance;
- 
     const main = getInputTextValueById('main-balance');
     const mainBalance = parseFloat(main);
-    if(amount > mainBalance)
+    if(amount > mainBalance || amount < 0)
         {
             alert('Wrong input.Try Again.');
             return ;
     
         }
+        const current = getInputTextValueById('available-balance');
+        const currentAmount = parseFloat(current);
+    
+        const balance = amount + currentAmount;
+        document.getElementById('available-balance').innerText = balance;
+
+     
     const currentBalance = mainBalance - donation;
     document.getElementById('main-balance').innerText = currentBalance;
- 
-   
- })
+
+    const modal = document.getElementById('my_modal_1');
+
+    if (currentAmount < balance) {
+        modal.showModal();
+    }
+    const closeModalButton = document.getElementById('close-modal-btn');
+    closeModalButton.addEventListener('click', function () {
+        modal.close();
+    });
+
+    makeHistory('card1', amount);
+    updateHistory();
+        
+
+    })
+
  
  
  document.getElementById('donation-btn2').addEventListener('click',function(){
      const donation2 = getInputFieldValueById('donation-amount2');
      const amount2 = parseFloat(donation2);
+     if(isNaN(amount2))
+        {
+            alert('Wrong input.Try Again.');
+            return ;
+        }
     
-     const current2 = getInputTextValueById('available-balance2');
-     const currentAmount2 = parseFloat(current2);
-     const balance2 = amount2 + currentAmount2;
-     document.getElementById('available-balance2').innerText = balance2;
+     
   
      const main = getInputTextValueById('main-balance');
      const mainBalance = parseFloat(main);
+     if(amount2 > mainBalance || amount2 < 0)
+        {
+            alert('Wrong input.Try Again.');
+            document.getElementById('mod-box2').classList.add('hidden');
+            return ;
+    
+        }
+        const current2 = getInputTextValueById('available-balance2');
+     const currentAmount2 = parseFloat(current2);
+     const balance2 = amount2 + currentAmount2;
+     document.getElementById('available-balance2').innerText = balance2;
      const currenBalance2 = mainBalance - donation2;
      document.getElementById('main-balance').innerText = currenBalance2;
+
+
+     const modal2 = document.getElementById('my_modal_5');
+
+     if (currentAmount2 < balance2) {
+         modal2.showModal();
+     }
+     const closeModalButton2 = document.getElementById('close-modal-btn2');
+     closeModalButton2.addEventListener('click', function () {
+         modal2.close();
+     });
+
+     makeHistory('card2', amount2);
+     updateHistory();
   
     
   })
@@ -50,32 +94,58 @@ document.getElementById('donation-btn').addEventListener('click',function(){
  document.getElementById('donation-btn3').addEventListener('click',function(){
      const donation3 = getInputFieldValueById('donation-amount3');
      const amount3 = parseFloat(donation3);
+     if(isNaN(amount3))
+        {
+            alert('Wrong input.Try Again.');
+            return ;
+        }
     
      const current3 = getInputTextValueById('available-balance3');
      const currentAmount3 = parseFloat(current3);
-     const balance3 = amount3 + currentAmount3;
-     document.getElementById('available-balance3').innerText = balance3;
-  
+     if (isNaN(currentAmount3)) {
+        alert('Available balance is invalid');
+        return;
+    }
      const main = getInputTextValueById('main-balance');
      const mainBalance = parseFloat(main);
-     const currenBalance3 = mainBalance - donation3;
-     document.getElementById('main-balance').innerText = currenBalance3;
+     if(amount3 > mainBalance || amount3 < 0)
+        {
+            alert('Wrong input.Try Again.');
+            document.getElementById('mod-box3').classList.add('hidden');
+            return ;
+    
+        }
+
+        const balance3 = amount3 + currentAmount3;
+     document.getElementById('available-balance3').innerText = balance3;
+  
+     const currentBalance3 = mainBalance - amount3;
+     document.getElementById('main-balance').innerText = currentBalance3;
+
+
+     const modal3 = document.getElementById('my_modal_6');
+
+     if (currentAmount3 < balance3) {
+         modal3.showModal();
+     }
+     const closeModalButton3 = document.getElementById('close-modal-btn3');
+     closeModalButton3.addEventListener('click', function () {
+         modal3.close();
+     });
+
+     makeHistory('card3', amount3);
+     updateHistory();
   
     
   })
- 
- 
-  document.getElementById('history-btn').addEventListener('click', function(){
-    document.getElementById('donation-card').classList.add('hidden');
+   
 
-   const historyDiv = document.createElement('div');
-   historyDiv.className = "border rounded-xl p-6 mt-5";
-    historyDiv.innerHTML = `
-        
-            <p class="font-bold mb-3">96500 Taka is Donated for famine-2024 at Feni, Bangladesh</p>
-            <p class="text-gray-500">${new Date().toLocaleDateString()}</p>
-    `;
-    document.getElementById('history').appendChild(historyDiv);
  
- });
+
  
+ 
+
+ 
+  
+
+  
--- js/features.js
+++ js/features.js
@@ -1,19 +1,72 @@
-//document.getElementById('show-donation').addEventListener('click',function(){
-   
-    //document.getElementById('history').classList.add('hidden');
-    //document.getElementById('donation-card').classList.remove('hidden');
-    //document.getElementById('show-donation').classList.add('bg-button-colore');
-    //document.getElementById('history-btn').classList.remove('bg-button-colore');
-//})
+
+const historyCards = {
+    card1 : [],
+    card2 : [],
+    card3 : []
+  };
+  function makeHistory(cardId, moneyAmount)
+  {
+    const date = new Date().toLocaleString();
+    historyCards[cardId].push({moneyAmount, date});
+  }
+  function updateHistory(){
+    const card1History = document.getElementById('card1-history');
+    const card2History = document.getElementById('card2-history');
+    const card3History = document.getElementById('card3-history');
 
 
 
-//document.getElementById('history-btn').addEventListener('click',function(){
+    card1History.innerHTML = '';
+    card2History.innerHTML = '';
+    card3History.innerHTML = '';
 
-   // document.getElementById('donation-card').classList.add('hidden');
-    //document.getElementById('show-donation').classList.remove('bg-button-colore');
-    //document.getElementById('show-donation').classList.add('border');
-   // document.getElementById('history').classList.remove('hidden');
-   // document.getElementById('history-btn').classList.add('bg-button-colore');
+    historyCards.card1.forEach((historyCard) => {
+        const div = document.createElement('div');
+        div.classList.add('border' , 'p-5' , 'rounded-xl', 'mb-3');
+        div.innerHTML = `
+        <p class="font-bold mb-3">${historyCard.moneyAmount} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+        <p>Date : ${historyCard.date}</p>
 
-//})
+        `;
+        card1History.appendChild(div);
+        
+    })
+    historyCards.card2.forEach((historyCard) => {
+        const div = document.createElement('div');
+        div.classList.add('border' , 'p-5' , 'rounded-xl', 'mb-3');
+        div.innerHTML = `
+        <p class="font-bold">${historyCard.moneyAmount} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+        <p>Date : ${historyCard.date}</p>
+
+        `;
+        card2History.appendChild(div);
+        
+    })
+    historyCards.card3.forEach((historyCard) => {
+        const div = document.createElement('div');
+        div.classList.add('border' , 'p-5' , 'rounded-xl', 'mb-3');
+        div.innerHTML = `
+        <p class="font-bold">${historyCard.moneyAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+        <p>Date : ${historyCard.date}</p>
+
+        `;
+        card3History.appendChild(div);
+        
+    })
+  }
+  document.getElementById('history-btn').addEventListener('click', function () {
+    document.getElementById('donation-card').classList.add('hidden');
+    document.getElementById('history').classList.remove('hidden');
+    document.getElementById('show-donation').classList.remove('bg-button-colore');
+    document.getElementById('history-btn').classList.add('bg-button-colore');
+
+    updateHistory(); 
+});
+
+document.getElementById('show-donation').addEventListener('click', function () {
+    document.getElementById('history').classList.add('hidden');
+    document.getElementById('donation-card').classList.remove('hidden');
+    document.getElementById('show-donation').classList.add('bg-button-colore');
+    document.getElementById('history-btn').classList.remove('bg-button-colore');
+
+});
